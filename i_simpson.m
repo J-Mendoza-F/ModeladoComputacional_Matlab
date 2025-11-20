73b972f971ad348e29b9a8025c4f866575bae2c7
J-Mendoza-F <jmmendozaf@unjbg.edu.pe>
2025-11-20 10:03:56 -0500

    g

--- i_simpson.m
+++ i_simpson.m
@@ -1,20 +1,26 @@
+function i_simpson()
+
 a=input('Límite inferior del intervalo de integración: ');
 b=input('Límite superior del intervalo de integración: ');
 n=input('Número de particiones: ');
 
 h=(b-a)/n;
 
+f=@(x)x.^2;
+
 s1=0;
 s2=0;
 
 for i=1:n-1
-    X(i)=X(0)+i*h;
+    X(i+1)=X(1)+i*h;
     if mod(i,2)==0
-        s2=s2+f(X(i));
+        s2=s2+f(X(i+1));
     else
-        s1=s1+f(X(i));
+        s1=s1+f(X(i+1));
     end
-    I=h/3[f(X(0))+4*s1+2*s2+f(X(n))];
 end
 
-fprint('La integral es: ', I);
+X(n+1)=b;
+
+I=h/3*(f(X(0))+4*s1+2*s2+f(X(n)));
+fprintf('La integral es: ', I);
