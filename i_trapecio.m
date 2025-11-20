c778131be1964d3dda7e9e7aed246f2cd7d1bd36
J-Mendoza-F <jmmendozaf@unjbg.edu.pe>
2025-11-20 09:52:30 -0500

    d

--- i_trapecio.m
+++ i_trapecio.m
@@ -1,16 +1,20 @@
+function i_trapecio()
+
 a=input('Límite inferior del intervalo de integración: ');
 b=input('Límite superior del intervalo de integración: ');
 n=input('Número de particiones: ');
 
 h=(b-a)/n;
+f=@(x)x.^2;
 
+X=zeros(1,n+1);
 s=0;
 
 for i=1:n-1
-    s=s+f(X(i));
+    s=s+f(X(i+1));
     X(i+1)=X(i)+h;
 end
 
-I=h/2[f(X(0))+2*s+f(X(n))];
+I=h/2[f(X(1))+2*s+f(X(n+1))];
 
-fprint('La integral es: ', I);
+fprintf('La integral es: %f\n', I);
